193b4466d9708f4e5e34e8bd5606976f770ed696
Rosarin Niyomsuntisuk <rosarinniyomsuntisuk@gmail.com>
2022-04-11 20:37:32 +0700

    test01

--- home02.js
+++ home02.js
@@ -1,7 +1,12 @@
 const http = require('http')
+const fs = require('fs')
 
 const app = http.createServer((req,res)=>{
-    res.writeHead(200, 'Weldone!')
-    res.write('Hi!')
-    res.end();
+    fs.readFile('./home01.html', (err, data)=>{
+        if(err) throw err;
+        res.writeHead(200, {'Content-Type': "text/html"})
+        res.write(data)
+        res.end();
+    })
+    
 }).listen(3000)
